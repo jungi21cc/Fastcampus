4590d3f32fa2579fcce31147779a37904ed3779a
jungi21cc <jungi21cc@gmail.com>
2018-01-22 14:25:13 +0900

    update

---
+++ WEEK2/dsm_f.py
@@ -0,0 +1,15 @@
+
+var = 1234
+
+def disp1(s):
+    print("dsm_f1: ", s)
+    
+def disp2(s):
+    print("dsm_f2: ", s)
+
+def disp3(s):
+    print("dsm_f3: ", s)
+    
+class calc:
+    def plus(self, *args):
+        return sum(args)    
