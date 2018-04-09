f6836b28d690bec0d5e8eece3357f5706c1afcf4
jungi21cc <jungi21cc@gmail.com>
2018-04-09 23:27:38 +0900

    REST API with Flask updated

--- Python_Applications/REST_API/flask_a/dss.py
+++ Python_Applications/REST_API/flask_a/dss.py
@@ -4,37 +4,39 @@ import pickle
 app = Flask(__name__)
 app.config.update(
     TEMPLATES_AUTO_RELOAD = True,
-
 )
 
 # load model
 models = {}
 def init():
-    with open('./models/classification.plk', 'rb') as f:
-        models['classification'] = pickle.load(f)
-
+    with open("./models/classification.plk", "rb") as f:
+        models["classification"] = pickle.load(f)
 init()
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/predict/', methods = ['POST'])
+
+@app.route("/predict/", methods=["POST"])
 def predict():
-    classification_list = ['정치', '경제', '사회', '생활/문화', '세계', 'IT/과학']
-    model = model['classification']
 
-    sentence = request.values.get('sentence')
+    classification_list = ["정치","경제","사회","생활/문화","세계","IT/과학"]
+    model = models["classification"]
+
+    sentence = request.values.get("sentence")
 
     predict_data = model.predict_proba([sentence])[0]
-    result = {'status':200, 'result':list(predict_data), 'category':classification_list}
+    result = {"status": 200, "result":list(predict_data), "category":classification_list}
+
     return jsonify(result)
 
+if __name__ == "__main__":
+    app.run(debug=True)
 
+# for windows
+# $ python3 dss.py
+# 127.0.0.1:5000
 
-
-if __name__ == '__main__':
-    app.run()
-
-#run
-#$ python3 gunicorn --reload dss:app
+# for mac
+# $ gunicorn --reload dss:app
